7f8bbbab0d953c99776aa633388d68538f5d91aa
Keesun Baik (a.k.a, Whiteship) <whiteship@epril.com>
2018-10-13 01:01:09 -0700

    polish

--- src/main/java/me/whiteship/natual/NaturalApplication.java
+++ src/main/java/me/whiteship/natual/NaturalApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class NatualApplication {
+public class NaturalApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NatualApplication.class, args);
+        SpringApplication.run(NaturalApplication.class, args);
     }
 
     @Bean
---
+++ src/test/java/me/whiteship/natual/NaturalApplicationTests.java
@@ -0,0 +1,11 @@
+package me.whiteship.natual;
+
+import org.junit.Test;
+
+public class NaturalApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
