e6ac9bd63ef076b192b53e7cc43543a77df71257
onjsdnjs <onjsdnjs@gmail.com>
2019-01-27 00:27:43 +0900

    Springboot Security Test

--- src/main/java/io/anymobi/common/listener/security/CacheManager.java
+++ src/main/java/io/anymobi/common/listener/security/CacheManager.java
@@ -4,32 +4,17 @@ import io.anymobi.domain.dto.security.AuthoritiesDto;
 import org.springframework.context.ApplicationListener;
 
 import java.util.List;
-import java.util.Map;
-
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.toList;
 
 public class CacheManager implements ApplicationListener<CacheEventMessage> {
 
-    //private Map<String, List<AuthoritiesDto>> authorities;
     private List<AuthoritiesDto> authorities;
-
-//    public Map<String, List<AuthoritiesDto>> getAuthorities() {
-//        return authorities;
-//    }
 
     public List<AuthoritiesDto> getAuthorities() {
         return authorities;
     }
 
-    //public List<AuthoritiesDto> getAuthoritie(String key) {
-        //return authorities.get(key);
-    //}
-
     @Override
     public void onApplicationEvent(CacheEventMessage event) {
-
-        //authorities = event.getAuthoritiesDto().stream().collect(groupingBy(AuthoritiesDto::getUrl, toList()));
         authorities = event.getAuthoritiesDto();
     }
 }
