b559a5ca5e81f1409d8cde2b702422c7a65cf95d
onjsdnjs <onjsdnjs@gmail.com>
2019-02-15 23:58:48 +0900

    test

--- src/main/java/io/anymobi/controller/rest/board/JpaBoardController.java
+++ src/main/java/io/anymobi/controller/rest/board/JpaBoardController.java
@@ -3,8 +3,10 @@ package io.anymobi.controller.rest.board;
 import io.anymobi.common.annotation.SocialUser;
 import io.anymobi.common.hateoas.HateoasHolder;
 import io.anymobi.common.hateoas.resources.BoardResource;
+import io.anymobi.common.hateoas.resources.EventResource;
 import io.anymobi.domain.dto.board.BoardDto;
 import io.anymobi.domain.entity.board.Board;
+import io.anymobi.domain.entity.event.Event;
 import io.anymobi.domain.entity.users.User;
 import io.anymobi.services.jpa.board.BoardService;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +17,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.PagedResources;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -24,6 +27,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.net.URI;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -42,7 +46,9 @@ public class JpaBoardController {
     }
 
     @GetMapping(value = "/boards", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity list(@PageableDefault Pageable pageable, PagedResourcesAssembler<Board> assembler, @SocialUser User user) {
+    public ResponseEntity list(@PageableDefault Pageable pageable,
+                               PagedResourcesAssembler<Board> assembler,
+                               @AuthenticationPrincipal User user) {
 
         if (user == null) {
             log.debug("Current user doesn't exist");
@@ -53,7 +59,7 @@ public class JpaBoardController {
         Page<Board> boardList = boardService.findBoardList(pageable);
         PagedResources<BoardResource> resources = assembler.toResource(boardList, entity -> new BoardResource(entity));
         resources.add(linkTo(this.getClass()).withRel("boards"));
-        resources.add(linkTo(methodOn(this.getClass()).getBoard(null)).withRel("get-an-board"));
+        resources.add(linkTo(methodOn(this.getClass()).getBoard(null,null)).withRel("get-an-board"));
 
         if (user != null) {
             resources.add(linkTo(methodOn(this.getClass()).createBoard(null, null, null)).withRel("create-new-board"));
@@ -64,17 +70,27 @@ public class JpaBoardController {
     }
 
     @GetMapping(value = "/boards/{idx}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity getBoard(@PathVariable Long idx) {
+    public ResponseEntity getBoard(@PathVariable Long idx,
+                                   @AuthenticationPrincipal User user) {
 
         Board board = boardService.findBoardByIdx(idx);
         if (board == null) {
             return HateoasHolder.notFoundResponse();
         }
-        return ResponseEntity.ok().body(board);
+
+        BoardResource boardResource = new BoardResource(board);
+        if (user != null) {
+            boardResource.add(linkToUpdate(board));
+        }
+        boardResource.add(HateoasHolder.linkToProfile("resources-boards-get"));
+
+        return ResponseEntity.ok().body(boardResource);
     }
 
     @PostMapping(value = "/boards", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity createBoard(@Valid @RequestBody BoardDto boardDto, BindingResult errors, @AuthenticationPrincipal User user) {
+    public ResponseEntity createBoard(@Valid @RequestBody BoardDto boardDto,
+                                      BindingResult errors,
+                                      @AuthenticationPrincipal User user) {
 
         if (errors.hasErrors()) {
             return HateoasHolder.badRequestResponse(errors);
@@ -90,14 +106,35 @@ public class JpaBoardController {
         boardResource.add(HateoasHolder.linkToProfile("resources-boards-create"));
         boardResource.add(linkToUpdate(newBoard));
 
-        URI newBoardLocation = linkTo(methodOn(this.getClass()).getBoard(newBoard.getIdx())).toUri();
+        URI newBoardLocation = linkTo(methodOn(this.getClass()).getBoard(newBoard.getIdx(),null)).toUri();
         return ResponseEntity.created(newBoardLocation).body(boardResource);
     }
 
     @PutMapping(value = "/boards/{idx}", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity update(@PathVariable Long idx, @Valid @RequestBody BoardDto baordDto, BindingResult errors, @AuthenticationPrincipal User user) {
-        
-        return ResponseEntity.ok().body(null);
+    public ResponseEntity update(@PathVariable Long idx,
+                                 @Valid @RequestBody BoardDto boardDto,
+                                 BindingResult errors,
+                                 @AuthenticationPrincipal User user) {
+
+        if (errors.hasErrors()) {
+            return HateoasHolder.badRequestResponse(errors);
+        }
+
+        Board board = this.boardService.findBoardByIdx(idx);
+        if (board == null) {
+            return HateoasHolder.notFoundResponse();
+        }
+
+        if (user != null) {
+            return new ResponseEntity(HttpStatus.FORBIDDEN);
+        }
+
+        modelMapper.map(boardDto, board);
+
+        BoardResource boardResource = new BoardResource(board);
+        boardResource.add(HateoasHolder.linkToProfile("resources-boards-update"));
+
+        return ResponseEntity.ok().body(boardResource);
     }
 
     public Link linkToUpdate(Board newBoard) {
