944ad42d4d3263ffcf6e592d6ecd86706f67a41e
onjsdnjs <onjsdnjs@gmail.com>
2019-02-19 22:06:31 +0900

    test

--- src/main/java/io/anymobi/controller/rest/SessionController.java
+++ src/main/java/io/anymobi/controller/rest/SessionController.java
@@ -1,15 +1,18 @@
 package io.anymobi.controller.rest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 
 @RestController
+@Slf4j
 public class SessionController {
 
     @GetMapping("/session")
     public String getSessionId(HttpSession session) {
+        log.debug(session.getId());
         return session.getId();
     }
 }
