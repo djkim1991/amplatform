ab4f3b0039d0c56429030dda5da903ce0093deae
onjsdnjs <onjsdnjs@gmail.com>
2019-03-04 18:10:21 +0900

    test

--- src/main/java/io/anymobi/AMPApplication.java
+++ src/main/java/io/anymobi/AMPApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
@@ -16,7 +15,6 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 import java.nio.charset.Charset;
 
 @Slf4j
-@Profile({"simple", "advanced", "event", "async-event", "aop-async-event", "distributed-aop-async-event"})
 @EnableAsync(proxyTargetClass = true)
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @EnableJpaRepositories(basePackages = "io.anymobi.repositories.jpa")
