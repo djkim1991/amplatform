68f0e8ccc97d2b4062bf071abd8eb5bb980d2970
onjsdnjs <onjsdnjs@gmail.com>
2019-03-05 09:11:36 +0900

    test

--- src/main/java/io/anymobi/configs/AppConfig.java
+++ src/main/java/io/anymobi/configs/AppConfig.java
@@ -1,5 +1,6 @@
 package io.anymobi.configs;
 
+import com.maxmind.geoip2.DatabaseReader;
 import io.anymobi.common.listener.security.AuthoritiesManager;
 import io.anymobi.common.validator.EmailValidator;
 import io.anymobi.common.validator.PasswordMatchesValidator;
@@ -13,8 +14,14 @@ import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
+import ua_parser.Parser;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
 
 @Configuration
 @EnableAsync(proxyTargetClass = true)
@@ -60,6 +67,20 @@ public class AppConfig {
     public RestOperations restTemplate() {
         RestTemplate restTemplate = new RestTemplate(this.clientHttpRequestFactory());
         return restTemplate;
+    }
+
+    @Bean
+    public Parser uaParser() throws IOException {
+        return new Parser();
+    }
+
+    @Bean
+    public DatabaseReader databaseReader() throws IOException {
+        File database = ResourceUtils
+                .getFile("classpath:maxmind/GeoLite2-City.mmdb");
+        return new DatabaseReader.Builder(database)
+                .locales(Collections.singletonList("ko_KR"))
+                .build();
     }
 
 }
